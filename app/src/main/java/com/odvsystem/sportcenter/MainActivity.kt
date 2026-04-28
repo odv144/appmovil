b107fe9d9ff0068c716ee48fc14c544dc7a2297e
odv144 <omar.virili@gmail.com>
2026-04-28 20:22:57 -0300

    subiendo proyecto inicial de app

---
+++ app/src/main/java/com/odvsystem/sportcenter/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.odvsystem.sportcenter
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val boton: Button = findViewById(R.id.btnLogin)
+
+        boton.setOnClickListener{
+
+            val intentar = Intent(this, MenuActivity::class.java)
+            startActivity(intentar)
+            Toast.makeText(this,"has precionado el boton", Toast.LENGTH_LONG).show()
+        }
+    }
+}
