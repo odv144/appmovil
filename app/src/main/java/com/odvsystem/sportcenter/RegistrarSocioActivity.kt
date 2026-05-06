081f8cd9574e095d28e0ba9e60eac8716d242aff
odv144 <omar.virili@gmail.com>
2026-05-05 23:48:28 -0300

    registrar no socio en blanco

--- app/src/main/java/com/odvsystem/sportcenter/RegistrarSocioActivity.kt
+++ app/src/main/java/com/odvsystem/sportcenter/RegistrarSocioActivity.kt
@@ -1,6 +1,8 @@
 package com.odvsystem.sportcenter
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.odvsystem.sportcenter.databinding.ActivityRegistrarSocioBinding
@@ -12,8 +14,14 @@ class RegistrarSocioActivity : AppCompatActivity() {
         binding = ActivityRegistrarSocioBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge()
+        val guardar: Button= findViewById<Button>(R.id.btnGuardar)
         binding.encabezado.setTitulo("Nuevo Socio")
         binding.encabezado.setDestino(MenuActivity::class.java)
+
+        guardar.setOnClickListener {
+            val intentar = Intent(this, MenuActivity::class.java)
+            startActivity(intentar)
+        }
 
         
 
