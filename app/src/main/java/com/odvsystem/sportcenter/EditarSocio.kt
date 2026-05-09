537cb5566b545a3ea42e461932593422e915ab43
JNCallaGiron <jairocalla@gmail.com>
2026-05-09 19:40:17 -0300

    pantalla socio y portada

---
+++ app/src/main/java/com/odvsystem/sportcenter/EditarSocio.kt
@@ -0,0 +1,36 @@
+package com.odvsystem.sportcenter
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.odvsystem.sportcenter.databinding.ActivityEditarSocioBinding
+
+class EditarSocio : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityEditarSocioBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditarSocioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+        val guardar: Button= findViewById<Button>(R.id.btnGuardar)
+        val limpiar: Button= findViewById<Button>(R.id.btnLimpiar)
+        binding.encabezado.setTitulo("Editar Socio")
+        binding.encabezado.setDestino(SociosActivity::class.java)
+
+        guardar.setOnClickListener {
+            val intentar = Intent(this, SociosActivity::class.java)
+            startActivity(intentar)
+        }
+
+        limpiar.setOnClickListener {
+            val intentar = Intent(this, SociosActivity::class.java)
+            startActivity(intentar)
+        }
+
+        }
+    }
--- app/src/main/java/com/odvsystem/sportcenter/InicioPortada.kt
+++ app/src/main/java/com/odvsystem/sportcenter/InicioPortada.kt
@@ -3,21 +3,19 @@ package com.odvsystem.sportcenter
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class InicioPortada : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val boton: Button = findViewById(R.id.btnLogin)
+        setContentView(R.layout.activity_inicio_portada)
 
-        boton.setOnClickListener{
-            val intentar = Intent(this, MenuActivity::class.java)
+        val boton: Button = findViewById(R.id.btnIniciarSesion)
+
+        boton.setOnClickListener {
+            val intentar = Intent(this, MainActivity::class.java)
             startActivity(intentar)
         }
     }
